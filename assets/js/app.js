8a1e35bb851261d523d4ae438fd8ef58c8d9b28d
GopalAror <punjabijanu586@gmail.com>
2023-10-05 11:02:28 +0530

    update

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+let mobileView = document.querySelector(".mobileView")
+let menu = document.querySelector("#menu")
+let span4 = document.querySelector(".span4")
+let span5 = document.querySelector(".span5")
+let span6 = document.querySelector(".span6")
+let home  = document.querySelector(".home")
+let sale = document.querySelector(".sale")
+let count = 1
+let count_no= document.getElementById("count_no")
+let sub = document.getElementById("sub")
+let add = document.getElementById("add")
+
+menu.addEventListener("click",function(){
+  mobileView.classList.toggle("show");
+  document.querySelector("body").classList.toggle("overflow-hidden");
+  span4.classList.toggle("span1");
+  span5.classList.toggle("span2");
+  span6.classList.toggle("span3");
+})
+home.addEventListener("click",function(){
+  mobileView.classList.remove("show");
+  document.querySelector("body").classList.remove("overflow-hidden");
+  span4.classList.remove("span1");
+  span5.classList.remove("span2");
+  span6.classList.remove("span3");
+})
+sale.addEventListener("click",function(){
+  mobileView.classList.remove("show");
+  document.querySelector("body").classList.remove("overflow-hidden");
+  span4.classList.remove("span1");
+  span5.classList.remove("span2");
+  span6.classList.remove("span3");
+})
+function opentab(tabId) {
+  mycontent = document.querySelectorAll(".tab-text")
+  mycontent.forEach(Mycontent => {
+      Mycontent.classList.add("d_none")
+      Mycontent.classList.remove("d_block")
+      
+  });
+  const opentab = document.getElementById(tabId);
+  opentab.classList.add("d_block");
+        };
+        opentab('tab1') ;
+
+
+        add.addEventListener("click", function(){
+          count++;
+          count_no.innerHTML = count;
+        })
+        sub.addEventListener("click", function(){
+          if( count > 1 ) count--;
+          count_no.innerHTML = count;
+        })
+
+        const preloader = document.getElementById("preloader")
+setTimeout(() => {
+        preloader.classList.add("d-none")
+        preloader.classList.add("pointer-event-none")
+        document.body.classList.remove("overflow-hidden")
+    }, 2000)
